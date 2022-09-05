b76c5f283c2c91d54476b49dc307f0fe9de797aa
KoganLior <liorrrr100@gmail.com>
2022-09-05 23:38:24 +0300

    Now you can get a product image

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { ProductImgService } from "./productImg.service";
 import { ProductsController } from "./products.controller";
 import { ProductsService } from "./products.service";
 
 @Module({
     imports:[],
-    providers:[ProductsService],
+    providers:[ProductsService,ProductImgService],
     controllers:[ProductsController]
 })
 export class ProductsModule{}
